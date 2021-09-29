16f1619ece1a9fd8ee77d7c935eeba2b09f24b4e
giulio <>
2021-09-29 13:16:11 +0200

    esercizio persona
    non finito

---
+++ Persona/Persona.java
@@ -0,0 +1,21 @@
+public class Persona{
+	
+	//attributi
+	private String nome;
+	private int eta;
+	
+	//costruttori
+	Persona(){
+		nome = "";
+		eta = 0;
+	}
+	Persona(String nome, int eta){
+		this.nome = nome;
+		this.eta = eta;
+	}
+	
+	//getter setter
+	
+	
+	
+}
